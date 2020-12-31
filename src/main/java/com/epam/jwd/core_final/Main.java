c8890b87ebbdeb29b12282844467cf2bcd8ebc62
romanoidprg <romanoidprg@mail.ru>
2020-12-31 13:58:49 +0300

    Configurated log4j

--- src/main/java/com/epam/jwd/core_final/Main.java
+++ src/main/java/com/epam/jwd/core_final/Main.java
@@ -1,10 +1,18 @@
 package com.epam.jwd.core_final;
 
 import com.epam.jwd.core_final.context.Application;
+import com.epam.jwd.core_final.exception.InvalidStateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class Main {
-
+    final static Logger logger =LoggerFactory.getLogger(com.epam.jwd.core_final.Main.class);
     public static void main(String[] args) {
-        Application.start();
+        try {
+            Application.start();
+        } catch (InvalidStateException e) {
+            logger.error(e.getMessage());
+        }
     }
 }
