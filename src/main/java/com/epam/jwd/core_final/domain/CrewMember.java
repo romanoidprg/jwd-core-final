099638a520586fa0290daa41e0deb5378212688e
romanoidprg <romanoidprg@mail.ru>
2020-12-31 14:36:55 +0300

    CrewMember class update

--- src/main/java/com/epam/jwd/core_final/domain/CrewMember.java
+++ src/main/java/com/epam/jwd/core_final/domain/CrewMember.java
@@ -9,4 +9,43 @@ package com.epam.jwd.core_final.domain;
  */
 public class CrewMember extends AbstractBaseEntity {
     // todo
+    private Role role;
+    private Rank rank;
+    private boolean isReadyForNextMissions;
+    private final String memberName;
+
+    public CrewMember(Role role, String memberName, Rank rank) {
+        this.role = role;
+        this.rank = rank;
+        isReadyForNextMissions = true;
+        this.memberName = memberName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public boolean isReadyForNextMissions() {
+        return isReadyForNextMissions;
+    }
+
+    public void setReadyForNextMissions(boolean readyForNextMissions) {
+        isReadyForNextMissions = readyForNextMissions;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
 }
